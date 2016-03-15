0b20bdfafdecbeb4d5692e1d9f5e551745f79c1d
Jean Cheever <jeancheever@Jeans-MacBook-Air.local>
2016-03-15 16:35:50 -0500

    8.7.4 GROUP BY EXERCISE STEP #6

--- group_by_exercise.sql
+++ group_by_exercise.sql
@@ -36,3 +36,19 @@ FROM employees
 WHERE last_name LIKE 'E%'
 AND last_name LIKE '%E'
 GROUP BY first_name, last_name;
+
+-- Step #6: Find the unique last names with a 'q' but not 'qu'. 
+-- You may use either DISTINCT or GROUP BY. Your results should be:
+-- Chleq
+-- Lindqvist
+-- Qiwen
+SELECT last_name
+FROM employees
+WHERE last_name LIKE '%q%'
+AND last_name NOT LIKE '%qu%'
+GROUP BY last_name;
+
+
+
+
+
