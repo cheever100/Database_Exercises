dddffb46671d7e88dcf91a819337578ae9204cb4
Jean Cheever <jeancheever@Jeans-MacBook-Air.local>
2016-03-15 14:38:59 -0500

    8.7.2 WHERE EXERCISE STEP #2

--- where_exercises.sql
+++ where_exercises.sql
@@ -3,24 +3,28 @@ USE codeup_test_db;
 -- Employees with first names 'Irena', 'Vidya', or 'Maya' — 709 rows (Hint: Use IN).
 SELECT first_name, last_name
 FROM employees
-WHERE first_name IN ('Irena', 'Vidya', 'Maya');
+-- WHERE first_name IN ('Irena', 'Vidya', 'Maya');
+-- Update your query for 'Irena', 'Vidya', or 'Maya' to use OR instead of IN — 709 rows.
+WHERE first_name = 'Irena'
+OR first_name = 'Vidya'
+OR first_name = 'Maya';
 
--- Employees whose last name starts with 'E' — 7,330 rows.
-SELECT first_name, last_name
-FROM employees
-WHERE last_name LIKE 'E%';
+-- -- Employees whose last name starts with 'E' — 7,330 rows.
+-- SELECT first_name, last_name
+-- FROM employees
+-- WHERE last_name LIKE 'E%';
 
--- Employees hired in the 90s — 135,214 rows.
-SELECT first_name, last_name, hire_date
-FROM employees
-WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31';
+-- -- Employees hired in the 90s — 135,214 rows.
+-- SELECT first_name, last_name, hire_date
+-- FROM employees
+-- WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31';
 
--- Employees born on Christmas — 842 rows.
-SELECT first_name, last_name, birth_date
-FROM employees
-WHERE birth_date LIKE '%12-25%';
+-- -- Employees born on Christmas — 842 rows.
+-- SELECT first_name, last_name, birth_date
+-- FROM employees
+-- WHERE birth_date LIKE '%12-25%';
 
--- Employees with a 'q' in their last name — 1,873 rows.
-SELECT first_name, last_name
-FROM employees
-WHERE last_name LIKE '%q%';
+-- -- Employees with a 'q' in their last name — 1,873 rows.
+-- SELECT first_name, last_name
+-- FROM employees
+-- WHERE last_name LIKE '%q%';
