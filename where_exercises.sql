c45b7f9a3659a27e06538fde8d282dec0874eded
Jean Cheever <jeancheever@Jeans-MacBook-Air.local>
2016-03-15 15:09:28 -0500

    8.7.2 WHERE EXERCISE STEP #7

--- where_exercises.sql
+++ where_exercises.sql
@@ -31,12 +31,14 @@ USE codeup_test_db;
 
 -- Employees born on Christmas — 842 rows.
 -- Find all employees hired in the 90s and born on Christmas — 362 rows.
-SELECT first_name, last_name, hire_date, birth_date
-FROM employees
-WHERE hire_date LIKE '%199%' 
-AND birth_date LIKE '%12-25%';
-
--- -- Employees with a 'q' in their last name — 1,873 rows.
--- SELECT first_name, last_name
+-- SELECT first_name, last_name, hire_date, birth_date
 -- FROM employees
--- WHERE last_name LIKE '%q%';
+-- WHERE hire_date LIKE '%199%' 
+-- AND birth_date LIKE '%12-25%';
+
+-- Employees with a 'q' in their last name — 1,873 rows.
+-- Find all employees with a 'q' in their last name but not 'qu' — 547 rows.
+SELECT first_name, last_name
+FROM employees
+WHERE last_name LIKE '%q%'
+AND last_name NOT LIKE '%qu%';
