f91243f70e5856b06d2a6e05cf7905de58f11b07
Jean Cheever <jeancheever@Jeans-MacBook-Air.local>
2016-03-15 16:23:06 -0500

    8.7.4 GROUP BY EXERCISE STEP #4

--- group_by_exercise.sql
+++ group_by_exercise.sql
@@ -13,3 +13,17 @@ USE codeup_test_db;
 SELECT DISTINCT title
 FROM titles
 ORDER BY title ASC;
+
+-- Step # 4: employees whose last names start and end with 'E'. 
+-- find just the unique last names that start and end with 'E'
+-- using GROUP BY. The results should be:
+ -- Eldridge
+ -- Erbe
+ -- Erde
+ -- Erie
+ -- Etalle
+SELECT last_name
+FROM employees
+WHERE last_name LIKE 'E%'
+AND last_name LIKE '%E'
+GROUP BY last_name;
