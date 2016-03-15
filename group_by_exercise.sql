0ca155801fa8e51a51f24efdb6d51b7aaa2d25a6
Jean Cheever <jeancheever@Jeans-MacBook-Air.local>
2016-03-15 16:10:53 -0500

    8.7.4 GROUP BY EXERCISE STEP #2

---
+++ group_by_exercise.sql
@@ -0,0 +1,12 @@
+USE codeup_test_db;
+
+-- use DISTINCT to find the unique titles in the titles table.
+--  Senior Engineer
+--  Staff
+--  Engineer
+--  Senior Staff
+--  Assistant Engineer
+--  Technique Leader
+--  Manager
+SELECT DISTINCT title
+FROM titles;
