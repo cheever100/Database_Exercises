9cd25624ecd995c64a33c7959a2e380ea9f23497
Jean Cheever <jeancheever@Jeans-MacBook-Air.local>
2016-03-16 09:43:00 -0500

    8.7.5 LIMIT EXERCISE Step #4

--- limit_exercises.sql
+++ limit_exercises.sql
@@ -15,3 +15,15 @@ WHERE hire_date LIKE '%199%'
 AND birth_date LIKE '%12-25%'
 ORDER BY birth_date ASC, hire_date DESC
 LIMIT 5;
+
+-- Step # 4:
+-- Try to think of your results as batches, or sets.
+-- The first five results are your first batch. 
+-- The five after that would be your second batch, etc.
+-- Update the query to find the tenth batch of results.
+SELECT first_name, last_name
+FROM employees
+WHERE hire_date LIKE '%199%'
+AND birth_date LIKE '%12-25%'
+ORDER BY birth_date ASC, hire_date DESC
+LIMIT 5 OFFSET 50;
