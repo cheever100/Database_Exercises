e1466b96c929d8e22f1e10f195815803e850d873
Jean Cheever <jeancheever@Jeans-MacBook-Air.local>
2016-03-16 10:55:02 -0500

    8.8. FUNCTIONS Exercise Step#4, corrections on Step #3

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -18,11 +18,26 @@ GROUP BY gender;
 -- Update your queries for employees whose names start and end with 'E'.
 -- Use concat() to combine their first and last name together as a single
 -- column in your results.
-SELECT CONCAT(first_name, ' ', last_name)
+SELECT CONCAT(first_name, ' ', last_name) AS 'Full Name'
 FROM employees
 WHERE (first_name = 'Irena'
 OR first_name = 'Vidya'
 OR first_name = 'Maya')
 AND gender = 'M'
 AND last_name LIKE '%E%'
-GROUP BY CONCAT(first_name, last_name); 
+GROUP BY 'Full Name'; 
+
+-- Step #4:
+-- For your query of employees born on Christmas and hired in the 90s
+-- use datediff() to find how many days they have been working at the company
+-- need to use now() or curdate()
+SELECT CONCAT(first_name, ' ', last_name) AS 'Full Name', DATEDIFF(CURDATE(), hire_date) AS 'Days at Company'
+FROM employees
+WHERE hire_date LIKE '%199%'
+AND birth_date LIKE '%12-25%'
+GROUP BY 'Full Name', 'Days at Company';
+
+
+
+
+
