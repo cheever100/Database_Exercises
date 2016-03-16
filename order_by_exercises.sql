d955728507341fcf400d8fb993e40c6b618802fc
Jean Cheever <jeancheever@Jeans-MacBook-Air.local>
2016-03-15 20:26:46 -0500

    8.7.3 ORDER BY EXERCISE Step #3

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -14,3 +14,14 @@ OR first_name = 'Maya')
 AND gender = 'M'
 ORDER BY first_name ASC; 
 
+-- Step #3:
+-- Update the query to order by first name and then last name.
+-- first result should now be Irena Acton
+-- last should be Vidya Zweizig
+SELECT first_name, last_name, gender
+FROM employees
+WHERE (first_name = 'Irena'
+OR first_name = 'Vidya'
+OR first_name = 'Maya')
+AND gender = 'M'
+ORDER BY first_name, last_name ASC; 
